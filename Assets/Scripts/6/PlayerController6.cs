18a17bce010312df20dedf7c274a22fca1940a7b
jiexiaka333 <3097914173@qq.com>
2026-03-24 08:09:57 +0800

    Day8: 完成音效系统和粒子系统入门

--- Assets/Scripts/6/PlayerController6.cs
+++ Assets/Scripts/6/PlayerController6.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 3f;      // 前后移动速度
     public float rotateSpeed = 100f;  // 旋转速度（度/秒）
+
     private Animator _animator;
 
+    public AudioSource bgmSource;
+    public AudioClip footstepClip;   // 脚步声
+
+    public ParticleSystem flameEffect;
+    public ParticleSystem explosionPrefab;
+
+   
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -28,12 +37,33 @@ public class PlayerController : MonoBehaviour
         if (v != 0)
         {
             // 沿着物体的前方移动（v 为正时向前，为负时向后）
-            Vector3 move = transform.forward * v * moveSpeed * Time.deltaTime;
+            Vector3 move =  new Vector3(1,0,0)* v * moveSpeed * Time.deltaTime;
             transform.Translate(move, Space.Self);
         }
+        // 按 F 键开关火焰
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (flameEffect.isPlaying)
+                flameEffect.Stop();
+            else
+                flameEffect.Play();
+        }
 
+        // 按 E 键播放爆炸
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ParticleSystem exp = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            exp.Play();
+            Destroy(exp.gameObject, exp.main.duration);
+        }
         // ===== 3. 动画参数 =====
         // 用 v 的绝对值表示移动速度（只关心前后，不管方向）
+
+        /*Mathf.Abs(v) 取绝对值，把负值变成正值
+
+例如：v = -0.5 → moveSpeedForAnim = 0.5
+
+为什么取绝对值？因为动画通常不分前后，无论是向前走还是向后走，用的都是同一套走路动画（只是角色面朝方向不同）*/
         float moveSpeedForAnim = Mathf.Abs(v);
         _animator.SetFloat("Speed", moveSpeedForAnim);
     }
@@ -41,6 +71,7 @@ public class PlayerController : MonoBehaviour
     // 动画事件调用的方法
     public void Footstep()
     {
-        Debug.Log("👣 脚步声");
+        bgmSource.PlayOneShot(footstepClip);
+        Debug.Log("播放");
     }
 }
